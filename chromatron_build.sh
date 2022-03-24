3ad7f054c1c11929d4d6798fed9a71186047838a
JeremyB <jeremybillheimer@gmail.com>
2022-03-23 19:06:54 -0500

    build scripts

---
+++ chromatron_build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# the ESP-IDF needs to be checked out in chromatron/src/hal
+# don't forget to setup submodules!
+# git submodule init
+# git submodule update
+
+./install.sh
+. ./export.sh
+
+pushd examples/build_system/cmake/idf_as_lib
+./build-esp32.sh
+popd
+
+mkdir ../esp32/esp-idf
+cp -r examples/build_system/cmake/idf_as_lib ../esp32/esp-idf
+cp -r components ../esp32/esp-idf
+cp esp32_out.ld ../esp32/esp-idf/idf_as_lib/build
