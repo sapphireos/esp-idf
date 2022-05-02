3d74cb6efb6e9b8d58df3fa5d900cd5f661459ec
JeremyB <jeremybillheimer@gmail.com>
2022-05-02 16:03:04 -0500

    update script

--- chromatron_build.sh
+++ chromatron_build.sh
@@ -13,10 +13,10 @@ pushd chromatron_lib
 ./build-esp32.sh
 popd
 
-mkdir ../esp32/esp-idf
-# cp -r examples/build_system/cmake/idf_as_lib ../esp32/esp-idf
-cp -r chromatron_lib ../esp32/esp-idf
-mv ../esp32/esp-idf/chromatron_lib ../esp32/esp-idf/idf_as_lib
-cp -r components ../esp32/esp-idf
-# cp esp32_chromatron.ld ../esp32/esp-idf/idf_as_lib/build
-cp esp32_chromatron.ld ../esp32/esp-idf/idf_as_lib/build
+mkdir ../esp32/esp-idf-single
+# cp -r examples/build_system/cmake/idf_as_lib ../esp32/esp-idf-single
+cp -r chromatron_lib ../esp32/esp-idf-single
+mv ../esp32/esp-idf-single/chromatron_lib ../esp32/esp-idf-single/idf_as_lib
+cp -r components ../esp32/esp-idf-single
+# cp esp32_chromatron.ld ../esp32/esp-idf-single/idf_as_lib/build
+cp esp32_chromatron.ld ../esp32/esp-idf-single/idf_as_lib/build
