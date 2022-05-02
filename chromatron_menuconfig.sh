49393958a0498a4c9e168eb9059a94b536797a5c
JeremyB <jeremybillheimer@gmail.com>
2022-05-02 16:00:08 -0500

    chromatron updates from 4.4.0

---
+++ chromatron_menuconfig.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+./install.sh
+. ./export.sh
+
+# pushd examples/build_system/cmake/idf_as_lib
+pushd chromatron_lib
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32.cmake -DTARGET=esp32 -G "Unix Makefiles"
+make -C build menuconfig
+popd
